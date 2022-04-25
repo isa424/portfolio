51101d6fc41240fce9f1928790543b37357480bd
Isa Ishangulyyev <isaishan424@gmail.com>
2022-04-25 22:07:35 +0300

    feat(ui): Update socials and tech pages

--- components/technical.js
+++ components/technical.js
@@ -15,10 +15,12 @@ const techs = [
 
 // CTRL + Left Mouse Click
 const work = [
+	"https://github.com/isa424/portfolio", // Portfolio source code
+	"https://github.com/isa424/isa424.github.io", // Ping pong with P5.js
+	"https://github.com/isa424/weather_forecast", // Weather forecast
+	"https://github.com/isa424/pokedex", // Pokemon Wiki App
 	"https://github.com/isa424/design_system", // Storybook design system
 	"https://github.com/isa424/react-todo", // Very basic todo app
-	"https://github.com/isa424/portfolio", // Portfolio code
-	"https://isa424.github.io/public", // Ping pong with P5.js
 	"works in progress..."
 ];
 `;
